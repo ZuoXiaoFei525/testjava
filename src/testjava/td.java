c2a49a728c878750e36645030edafb9cd638f7b9
liusilong <1157867422@qq.com>
2015-05-26 10:54:15 +0800

    testjava

---
+++ src/testjava/td.java
@@ -0,0 +1,81 @@
+package testjava;
+
+import java.io.Console;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class td {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// String a = "2.3";
+		// String b = "3.2";
+		// DecimalFormat df = new DecimalFormat("#.00");
+		// System.out.println(df.format(Double.valueOf(a) * Double.valueOf(b)));
+		// System.out.println(df.format(Double.parseDouble(a)
+		// * Double.parseDouble(b)));
+		// int a =3/4;
+		// double b =3/4;
+		// System.out.println(a+"-----"+b);
+		// if (3 < 4) {
+		// return;
+		// }
+		// System.out.println("after return");
+
+		// Map<String, String> map = new HashMap<String, String>();
+		// map.put("aa", "jack");
+		// map.put("bb", "rose");
+		// Iterator it = map.keySet().iterator();
+		// while (it.hasNext()) {
+		// String key;
+		// key = (String) it.next();
+		// System.out.println("email-" + key + ":" + map.get(key));
+		// }
+		// int count=0;
+		// for(int i=0;i<=100;i++){
+		// System.out.println(count+=count++);
+		// }
+
+		for (int i = 0; i < 3; i++) {
+			switch (i) {
+			case 0:
+				System.out.println("1");
+				break;
+			case 1:
+				System.out.println("2");
+				break;
+			case 2:
+				System.out.println("3");
+				break;
+			}
+		}
+		// String a = "刘斯龙";
+		// String b = "abc";
+		// System.out.println("a.length:" + a.length() + "--b.length():"
+		// + b.length());
+		//
+		// byte[] byBuffer = new byte[200];
+		// byte[] byBuffer1 = new byte[200];
+		// byte[] byBuffer2 = new byte[200];
+		// byte[] byBuffer3 = new byte[200];
+		// String strInput = "我是字符串";
+		// String strEng = "abcde";
+		// try {
+		// byBuffer = strInput.getBytes("utf-8");
+		// byBuffer1 = strInput.getBytes("gbk");
+		// byBuffer2 = strEng.getBytes("utf-8");
+		// byBuffer3 = strEng.getBytes("gbk");
+		// System.out.println(byBuffer.length);
+		// System.out.println(byBuffer1.length);
+		// System.out.println(byBuffer2.length);
+		// System.out.println(byBuffer3.length);
+		// } catch (UnsupportedEncodingException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+	}
+
+}
